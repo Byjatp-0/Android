a6c6c1e06f8694d685449094754a9c5ae752b92d
Byjatp-0 <jasidonia@gmail.com>
2025-11-10 19:24:04 +0100

    2.3

---
+++ Actividad5_listas/app/src/main/java/com/example/actividad5_listas/Videojuego.java
@@ -0,0 +1,11 @@
+package com.example.actividad5_listas;
+
+public class Videojuego {
+    private String nombre;
+    private String imagen;
+
+    public Videojuego(String nombre, String imagen){
+        this.nombre = nombre;
+        this.imagen = imagen;
+    }
+}
